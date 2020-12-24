2e41cb404e06f5c4fe34f76dbcba749475c46db9
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:26:17 +0800

    Initial commit

---
+++ var1-1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	const int n = 25;
+	const int m = 30;
+	int k, l;
+	cout << "Enter coordinates of a point" << endl;
+	cin >> k >> l;
+	while (k >= n || l >= m) {
+		cout << "Wrong Input" << endl;
+		cin >> k >> l;
+	}
+	int array[n][m];
+	array[k][l] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (abs(l - j) > abs(k - i)) {
+				array[i][j] = abs(l - j) + 1;
+			}
+			else {
+				array[i][j] = abs(k - i) + 1;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << array[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	system("pause");
+}
